8099c3d5bfdff5bfc08150dc954aef011102c9f3
margo225 <160738297+margo225@users.noreply.github.com>
2024-09-16 13:37:02 +0700

    Add files via upload

---
+++ singleton/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+string s = "httplkj";
+Console.WriteLine(s.Substring(0,4));
---
+++ singleton/Server.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleton
+{
+    internal class ServerLazy
+    {
+        static ServerLazy uniqueInstance;
+        private static object syncRoot = new object();
+        static List<string> serversHTTP = new List<string>();
+        static List<string> serversHTTPS = new List<string>();
+        internal ServerLazy() { }
+        internal static ServerLazy Instance()
+        {
+            Thread.Sleep(500);
+            if (uniqueInstance == null)
+            {
+                lock (syncRoot)
+                {
+                    if (uniqueInstance == null)
+                        uniqueInstance = new ServerLazy();
+                }
+            }
+            return uniqueInstance;
+        }
+        internal static bool Add(string s)
+        {
+            if (s.Substring(0, 5) == "https")
+            {
+                foreach (string elem in serversHTTPS)
+                {
+                    if (elem == s) return false;
+                }
+                return true;
+            }
+            else 
+            {
+                if (s.Substring(0, 4) == "http")
+                {
+                    foreach (string elem in serversHTTP)
+                    {
+                        if (elem == s) return false;
+                    }
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        internal static List<string> GetServersHTTP()
+        { 
+            return serversHTTP;
+        }
+        internal static List<string> GetServersHTTPS()
+        {
+            return serversHTTPS;
+        }
+    }
+    internal class ServerEager
+    {
+        static ServerEager uniqueInstance = new ServerEager();
+        private static object syncRoot = new object();
+        static List<string> serversHTTP = new List<string>();
+        static List<string> serversHTTPS = new List<string>();
+        internal ServerEager() { }
+        internal static ServerEager Instance()
+        {
+            return uniqueInstance;
+        }
+        internal static bool Add(string s)
+        {
+            if (s.Substring(0, 5) == "https")
+            {
+                foreach (string elem in serversHTTPS)
+                {
+                    if (elem == s) return false;
+                }
+                return true;
+            }
+            else
+            {
+                if (s.Substring(0, 4) == "http")
+                {
+                    foreach (string elem in serversHTTP)
+                    {
+                        if (elem == s) return false;
+                    }
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        internal static List<string> GetServersHTTP()
+        {
+            return serversHTTP;
+        }
+        internal static List<string> GetServersHTTPS()
+        {
+            return serversHTTPS;
+        }
+    }
+}
