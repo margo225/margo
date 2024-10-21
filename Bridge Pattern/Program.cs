cc2388233387f614561a43eb0e34f3084302983f
margo225 <160738297+margo225@users.noreply.github.com>
2024-10-21 14:15:50 +0700

    Add files via upload

---
+++ Bridge Pattern/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Bridge_Pattern;
+
+Inotification email = new Email();
+Inotification sms = new SMS();
+notification text = new Text(sms);
+notification html = new HTML(email);
+Console.WriteLine(text.Notif("my notification for you"));
+Console.WriteLine(html.Notif("my notification for you"));
---
+++ Bridge Pattern/notification.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge_Pattern
+{
+    public interface Inotification
+    {
+        string Send(string message);
+    }
+    public class Email : Inotification
+    {
+        public string  Send (string message)
+        {
+            return $"Отправка Email: {message}";
+        }
+    }
+    public class SMS : Inotification
+    {
+        public string Send(string message)
+        {
+            return $"Отправка SMS: {message}";
+        }
+    }
+    public abstract class notification
+    {
+        protected Inotification notificationSend;
+        protected notification(Inotification notificationSend)
+        { 
+            this.notificationSend = notificationSend;
+        }
+        public abstract string Notif (string message);
+    }
+    public class Text : notification
+    {
+        public Text (Inotification notificationSend) : base(notificationSend) {}
+        public override string Notif(string message)
+        {
+            string s=notificationSend.Send(message+"\n(text)");
+            return s;
+        }
+    }
+    public class HTML : notification
+    {
+        public HTML(Inotification notificationSend) : base(notificationSend) { }
+        public override string Notif(string message)
+        {
+            string s = notificationSend.Send(message + "\n(html)");
+            return s;
+        }
+    }
+}
