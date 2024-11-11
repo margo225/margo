563d0715b16b2c141fc61b30889e0f6024eaa389
margo225 <160738297+margo225@users.noreply.github.com>
2024-11-11 13:45:33 +0700

    Add files via upload

---
+++ Decorator Pattern/Notifier.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Decorator_Pattern
+{
+    public abstract class AbstractNotifier
+    {
+        public abstract string Display(); // возвращает text
+    }
+    public class Notifier : AbstractNotifier
+    {
+        protected string text;
+        public Notifier (string s)
+        {
+            text ='"' + s + '"';
+        }
+        public override string Display()
+        {
+            return text;
+        }
+    }
+    public abstract class Decorator : AbstractNotifier
+    {
+        public AbstractNotifier notifier { protected get; set; }
+        public override string Display()
+        {
+            if (notifier != null)
+                return notifier.Display();
+            else return "Ошибка";
+        }
+    }
+    public class Email : Decorator
+    {
+        string AddEmail(string text)
+        {
+            string s = "Отправляем на email         ";
+            return s + text;
+        }
+        public override string Display()
+        {
+            return AddEmail(base.Display());
+        }
+    }
+    public class SMS : Decorator
+    {
+        string AddSMS(string text)
+        {
+            string s = "Отправляем SMS         ";
+            return s + text;
+        }
+        public override string Display()
+        {
+            return AddSMS(base.Display());
+        }
+    }
+    public class Facebook : Decorator
+    {
+        string AddFacebook(string text)
+        {
+            string s = "Отправляем на Facebook         ";
+            return s + text;
+        }
+        public override string Display()
+        {
+            return AddFacebook(base.Display());
+        }
+    }
+}
---
+++ Decorator Pattern/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using Decorator_Pattern;
+
+string text = "Новое соощение";
+AbstractNotifier notifier = new Notifier(text);
+Decorator email = new Email();
+Decorator sms = new SMS();
+Decorator facebook = new Facebook();
+email.notifier = notifier;
+sms.notifier = email;
+facebook.notifier = sms;
+Console.WriteLine(facebook.Display());
