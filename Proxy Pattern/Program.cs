6dfa1c7622418449a7e153e798dd61e3f546588b
margo225 <160738297+margo225@users.noreply.github.com>
2024-11-18 14:23:05 +0700

    Add files via upload

---
+++ Proxy Pattern/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using Proxy_Pattern;
+
+Subject subject = new Proxy();
+Console.WriteLine((subject as Proxy).Prava("Hi", "admin"));
---
+++ Proxy Pattern/Proxy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy_Pattern
+{
+    public interface Subject
+    {
+        string Request(string request);
+    }
+    public class RealSubject : Subject
+    {
+        public string Request(string request)
+        {
+            return request+" in RealSubject";
+        }
+    }
+    public class Proxy : Subject
+    {
+        RealSubject realSubject;
+        public string Request (string request)
+        {
+            if (realSubject == null)
+            {
+                realSubject = new RealSubject();
+            }
+            return realSubject.Request(request);
+        }
+        public string Prava(string request, string rights)
+        {
+            if (rights == "admin")
+                return Request(request);
+            else
+                return "У вас нет прав для использования этой функции";
+        }
+    }
+}
