984c2a503c671147d6b9cdfb1518888cefc59a40
margo225 <160738297+margo225@users.noreply.github.com>
2024-11-25 14:11:56 +0700

    Add files via upload

---
+++ Command Pattern/Command.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command_Pattern
+{
+    class ArithmeticUnit
+    {
+        public string Register { get; private set; }
+        private List <int> numbers = new List <int> ();
+        public void Run(char operationCode, string operand, int NewInt)
+        {
+            switch (operationCode)
+            {
+                case '+':
+                    if (NewInt==-1)
+                        Register += operand;
+                    else
+                    {
+                        Register = Register.Insert(numbers[numbers.Count-1], operand);
+                        numbers.RemoveAt(numbers.Count-1);
+                    }
+
+                    break;
+                case '-':
+                    if (Register.LastIndexOf(operand) > -1)
+                    {
+                        numbers.Add(Register.LastIndexOf(operand));
+                        Register = Register.Replace(operand, "");
+                    }
+                    else
+                        Console.WriteLine("Невозможно удалить текс, так как он отсутствует");
+                    break;
+            }
+        }
+    }
+    abstract class Command
+    {
+        protected ArithmeticUnit unit;
+        protected string operand;
+        public abstract void Execute();
+        public abstract void Undo();
+    }
+
+    class Add : Command
+    {
+        public Add(ArithmeticUnit unit, string operand)
+        {
+            this.unit = unit;
+            this.operand = operand;
+        }
+        public override void Execute()
+        {
+            unit.Run('+', operand, -1);
+        }
+        public override void Undo()
+        {
+            unit.Run('-', operand, 0);
+        }        
+    }
+    class Sub : Command
+    {
+        public Sub(ArithmeticUnit unit, string operand)
+        {
+            this.unit = unit;
+            this.operand = operand;
+        }
+        public override void Execute()
+        {
+            unit.Run('-', operand, -1);
+        }
+        public override void Undo()
+        {
+            unit.Run('+', operand, 0);
+        }
+    }
+    class ControlUnit
+    {
+        private List <Command> commands = new List <Command>();
+        private int current = 0;
+        public void StoreComand (Command command)
+        {
+            commands.Add (command);
+        }
+        public void ExecuteCommand()
+        {
+            commands[current].Execute();
+            current++;
+        }
+        public void Undo(int levels)
+        {
+            for (int i = 0; i < levels; i++)
+            {
+                if (current > 0)
+                {
+                    commands[--current].Undo();
+                }
+            }
+        }
+    }
+
+    class Editor
+    {
+        ArithmeticUnit arithmeticUnit;
+        ControlUnit controlUnit;
+        public Editor()
+        { 
+            arithmeticUnit = new ArithmeticUnit();
+            controlUnit = new ControlUnit();
+        }
+        private string Run(Command command)
+        {
+            controlUnit.StoreComand (command);
+            controlUnit.ExecuteCommand();
+            return arithmeticUnit.Register;
+        }
+        public string Add(string text)
+        {
+            return Run(new Add(arithmeticUnit, text));
+        }
+        public string Sub(string text)
+        {
+            return Run (new Sub(arithmeticUnit, text));
+        }
+        public string Undo(int levels)
+        {
+            controlUnit.Undo(levels);
+            return arithmeticUnit.Register;
+        }
+    }
+}
---
+++ Command Pattern/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using Command_Pattern;
+
+Editor editor = new Editor();
+string result = "";
+result = editor.Add("Hi! How are you?");
+Console.WriteLine(result);
+result = editor.Sub("Hi!");
+Console.WriteLine(result);
+result = editor.Undo(1);
+Console.WriteLine(result);
