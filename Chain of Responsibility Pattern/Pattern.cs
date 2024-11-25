c96e9513e8465b4992b9c9b9470834cf709d44ea
margo225 <160738297+margo225@users.noreply.github.com>
2024-11-25 14:53:48 +0700

    Add files via upload

---
+++ Chain of Responsibility Pattern/Pattern.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chain_of_Responsibility_Pattern
+{
+    class Request
+    {
+        public int Int;
+        public string operation;
+        public Request(int s1, string s2) 
+        {
+            Int = s1;
+            operation = s2;
+        }
+    }
+    abstract class Pattern
+    {
+        protected string operation;
+        public Pattern Successor { get; set; }
+        public abstract void HandleRequest(Request request,int Chislo);
+    }
+    class ConcreteHandlerAdd : Pattern 
+    {
+        public ConcreteHandlerAdd ()
+        {
+            operation = "Add";
+        }
+        public override void HandleRequest(Request request, int Chislo)
+        {
+            if (operation == request.operation)
+            {
+                string s = Chislo.ToString() + "+" + request.Int.ToString() + "=" + (Chislo + request.Int).ToString();
+                Console.WriteLine(s);
+            }
+            else if (Successor != null) 
+                Successor.HandleRequest(request, Chislo);
+        }
+    }
+    class ConcreteHandlerSub : Pattern
+    {
+        public ConcreteHandlerSub()
+        {
+            operation = "Sub";
+        }
+        public override void HandleRequest(Request request, int Chislo)
+        {
+            if (operation == request.operation)
+            {
+                string s = Chislo.ToString() + "-" + request.Int.ToString() + "=" + (Chislo - request.Int).ToString();
+                Console.WriteLine(s);
+            }
+            else if (Successor != null)
+                Successor.HandleRequest(request, Chislo);
+        }
+    }
+    class ConcreteHandlerMul : Pattern
+    {
+        public ConcreteHandlerMul()
+        {
+            operation = "Mul";
+        }
+        public override void HandleRequest(Request request, int Chislo)
+        {
+            if (operation == request.operation)
+            {
+                string s = Chislo.ToString() + "*" + request.Int.ToString() + "=" + (Chislo * request.Int).ToString();
+                Console.WriteLine(s);
+            }
+            else if (Successor != null)
+                Successor.HandleRequest(request, Chislo);
+        }
+    }
+    class ConcreteHandlerDiv : Pattern
+    {
+        public ConcreteHandlerDiv()
+        {
+            operation = "Div";
+        }
+        public override void HandleRequest(Request request, int Chislo)
+        {
+            if (operation == request.operation)
+            {
+                string s = Chislo.ToString() + "/" + request.Int.ToString() + "=" + (Chislo / request.Int).ToString();
+                Console.WriteLine(s);
+            }
+            else if (Successor != null)
+                Successor.HandleRequest(request, Chislo);
+        }
+    }
+}
---
+++ Chain of Responsibility Pattern/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using Chain_of_Responsibility_Pattern;
+
+ConcreteHandlerAdd add = new ConcreteHandlerAdd();
+ConcreteHandlerDiv div = new ConcreteHandlerDiv();
+ConcreteHandlerMul mul = new ConcreteHandlerMul();
+ConcreteHandlerSub sub = new ConcreteHandlerSub();
+add.Successor = div;
+div.Successor = mul;
+mul.Successor = sub;
+Request request = new Request(5, "Sub");
+add.HandleRequest(request, 2);
+
