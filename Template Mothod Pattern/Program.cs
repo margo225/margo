beada9fde3b80d86e8856b18fb575774ccdc5db9
margo225 <160738297+margo225@users.noreply.github.com>
2024-12-16 13:57:06 +0700

    Add files via upload

---
+++ Template Mothod Pattern/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using Template_Mothod_Pattern;
+
+StudentReportGenerator studentReportGenerator = new StudentReportGenerator();
+studentReportGenerator.GenerateReport();
+CourseReportGenerator courseReportGenerator = new CourseReportGenerator();
+courseReportGenerator.GenerateReport();
---
+++ Template Mothod Pattern/ReportGenerator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template_Mothod_Pattern
+{
+    public abstract class ReportGenerator
+    {
+        public void GenerateReport()
+        {
+            DataCollection();
+            DataProcessing();
+            FormattingAReport();
+            ExportReport();
+        }
+        public abstract void DataCollection();
+        public abstract void DataProcessing();
+        public abstract void FormattingAReport();
+        public abstract void ExportReport();
+    }
+    public class StudentReportGenerator : ReportGenerator
+    {
+        public override void DataCollection()
+        {
+            Console.WriteLine("Собранны двнные по студентам");
+        }
+        public override void DataProcessing()
+        {
+            Console.WriteLine("Обработаны данные по студентам");
+        }
+        public override void FormattingAReport()
+        {
+            Console.WriteLine("Отформатирован отчет о студентах");
+        }
+        public override void ExportReport()
+        {
+            Console.WriteLine("Экспортирован отчет о студентах");
+        }
+    }
+    public class CourseReportGenerator : ReportGenerator
+    {
+        public override void DataCollection()
+        {
+            Console.WriteLine("Собранны данные о курсах");
+        }
+        public override void DataProcessing()
+        {
+            Console.WriteLine("Обработаны данные о курсах");
+        }
+        public override void FormattingAReport()
+        {
+            Console.WriteLine("Отформатирован отчет о курсах");
+        }
+        public override void ExportReport()
+        {
+            Console.WriteLine("Экспортирован отчет о курсах");
+        }
+    }
+}
