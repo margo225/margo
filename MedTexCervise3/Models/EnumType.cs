f8d1cac0a17cde5fd785087354b377f3d1f94713
margo225 <160738297+margo225@users.noreply.github.com>
2025-02-24 12:09:50 +0700

    Add files via upload

---
+++ MedTexCervise3/Models/EnumType.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedTexCervise2.Models
+{
+    /*public class EnumType
+    {
+    }*/
+    public enum ModelOb
+    {
+        AB,
+        BC
+    }
+    public enum TupeOb
+    {
+        Рентген,
+        Узи
+    }
+    public enum TypeShapeStatus
+    { 
+        New_shape,
+        Process_repair,
+        Completed
+    }
+    public enum Role
+    {
+        User,
+        Admin,
+        Worker
+    }
+}
---
+++ MedTexCervise3/Models/Shape.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedTexCervise2.Models
+{
+    public class Shape
+    {
+        private static int _lastId = 0;
+        private int id;
+        public int Id 
+        {  
+            get
+            { 
+                return id; 
+            }
+            set
+            { 
+                id = value;
+                _lastId--;
+
+            } 
+        }
+        public DateTime DateAdd { get; set; } = DateTime.Now;
+        public DateTime DateDelivery { get; set; } = DateTime.MaxValue;
+        public TupeOb tupeOb { get; set; }
+        public ModelOb modelOb { get; set; }
+        public string description { get; set; }
+        public FIO FIO { get; set; }
+        public string phone { get; set; }
+        public User user { get; set; } = new User();
+        public TypeShapeStatus TypeShapeStatus { get; set; }
+        public Shape()
+        {
+            id = ++_lastId;
+        }
+        public override string ToString()
+        {
+            return $"Заявка №{id} от {DateAdd}\n" +
+                $"Доставка: {DateDelivery}\n" +
+                $"Тип оборудования: {tupeOb}\n" +
+                $"Модель оборудования: {modelOb}\n" +
+                $"Описание проблемы: {description}\n" +
+                $"ФИО заказчика: {FIO}\n" +
+                $"Статус: {TypeShapeStatus}\n"+
+                $"Рабочий: {user}";
+        }
+    }
+}
